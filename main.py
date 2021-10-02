a5763bfaa2a42811aba26c0257cc92c4ac4608ca
IssacRajaSekar <91720416+IssacRajaSekar@users.noreply.github.com>
2021-10-02 12:34:33 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+#print the greeting
+print("Welcome to the Code Word Generator.")
+#storing the city name and pet name in a variable
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+#printing the code word
+print("Your code word could be " + city_name + pet_name)
